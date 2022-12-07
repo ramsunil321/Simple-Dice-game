38daf4aa2e7356b3ed896a63b7e2cc9222759113
Ram Sunil <98600245+ramsunil321@users.noreply.github.com>
2022-12-07 17:41:52 +0530

    updated some queries
    you can use your names to play this game

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-var floderName = "./images/" + randomDiceImage
 
-document.querySelectorAll("img")[0].setAttribute("src",floderName)
+var randomDiceImage = "dice" + randomNumber1 + ".png"
+
+var randomsrcImage = "./images/" + randomDiceImage
+
+
+document.querySelectorAll("img")[0].setAttribute("src",randomsrcImage)
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-var floderName2 = "./images/" + randomDiceImage2
 
-document.querySelectorAll("img")[1].setAttribute("src",floderName2)
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"
 
+var randomsrcImage2 = "./images/" + randomDiceImage2
 
+document.querySelectorAll("img")[1].setAttribute("src",randomsrcImage2)
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent =  player1 + " wins"
+var player1 = prompt("Enter 1st player name:")
+var player2 = prompt("Enter 2nd player name:")
+
+document.querySelectorAll("p")[0].innerHTML = player1.toUpperCase()
+document.querySelectorAll("p")[1].innerHTML = player2.toUpperCase()
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML =  player1.toUpperCase() + " WON"
 }
-else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").textContent =  player2 + " wins"
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = player2.toUpperCase() + " WON"
 }
 else{
-    document.querySelector("h1").textContent = "Draw"
+    document.querySelector("h1").innerHTML = "Draw"
 }
